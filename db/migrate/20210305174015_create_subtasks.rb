91d9fc48510cf7e1cb64d5887b7268813a32f540
Taylor Collins <taylorcc33@gmail.com>
2021-03-05 10:43:42 -0700

    models added

---
+++ db/migrate/20210305174015_create_subtasks.rb
@@ -0,0 +1,10 @@
+class CreateSubtasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :subtasks do |t|
+      t.string :name
+      t.belongs_to :task, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
